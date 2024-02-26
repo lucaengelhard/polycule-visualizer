4b2317082d6d403d716665dce4b903dfecc62d9c
Luca Engelhard <me@lucaengelhard.com>
2024-02-27 00:14:09 +0100

    simulation v2

--- src/graph/Graph.tsx
+++ src/graph/Graph.tsx
@@ -11,130 +11,82 @@ import * as d3 from "d3";
 import * as Types from "../types/types";
 import { clamp } from "../utils/helpers";
 
-export default function Graph({
-  graph,
-  setGraphState,
-}: {
-  graph: Types.GraphData;
-  setGraphState: Dispatch<SetStateAction<Types.DBData>>;
-}) {
-  const [width, setWidth] = useState(window.innerWidth);
-  const [height, setHeight] = useState(window.innerHeight);
+export default function Graph({ graph }: { graph: Types.GraphData }) {
+  const svgRef = useRef(null);
+  const wrapperRef = useRef(null);
 
-  const [simulation, setSimulation] = useState(
-    d3
-      .forceSimulation(graph.nodes)
+  useEffect(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    if (!width || !height) return;
+
+    const svg = d3.select(svgRef.current);
+
+    const nodes = graph.nodes.map((node) => node);
+    const links = graph.links.map((link) => link);
+
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force("charge", d3.forceManyBody().strength(-3000))
+      .force("center", d3.forceCenter(width / 2, height / 2))
       .force(
         "link",
         d3
-          .forceLink(graph.links)
-          .id((d) => d.id)
+          .forceLink(links)
+          .id((d: any) => d.id)
           .distance((d) => clamp(d.distance, 0, 500)),
       )
-      .force("charge", d3.forceManyBody().strength(-3000))
-      .force("center", d3.forceCenter(width / 2, height / 2))
       .force("x", d3.forceX())
       .force("y", d3.forceY())
       .force(
         "radial",
-        d3.forceRadial(Math.min(width, height) - 300, width / 2, height / 2),
-      ),
-  );
+        d3.forceRadial(Math.min(width, height) - 500, width / 2, height / 2),
+      )
+      .on("tick", () => {
+        svg
+          .selectAll(".link")
+          .data(links)
+          .join("line")
+          .attr("class", "link")
+          .attr("stroke", (link) => link.type.color)
+          .attr("stroke-width", "3")
+          .attr("fill", "none")
+          .attr("x1", (link) => link.source.x)
+          .attr("y1", (link) => link.source.y)
+          .attr("x2", (link) => link.target.x)
+          .attr("y2", (link) => link.target.y);
+
+        svg
+          .selectAll(".node")
+          .data(nodes)
+          .join("circle")
+          .attr("class", "node pointer-events-auto")
+          .attr("r", 8)
+          .attr("cx", (node) => node.x)
+          .attr("cy", (node) => node.y);
+
+        svg
+          .selectAll(".label")
+          .data(nodes)
+          .join("text")
+          .attr("class", "label")
+          .attr("text-anchor", "middle")
+          .attr("font-size", 20)
+          .text((node) => node.name)
+          .attr("x", (node) => node.x)
+          .attr("y", (node) => node.y)
+          .attr("dx", 21)
+          .attr("dy", -10);
+      });
+  }, [graph]);
 
   return (
-    <svg className="pointer-events-none fixed inset-0 h-screen w-screen">
-      <g>
-        {graph.links.map((link, index) => {
-          const source = graph.nodes.find((node) => {
-            if (typeof link.source === "number") {
-              return node.id === link.source;
-            } else {
-              return node.id === link.source.id;
-            }
-          });
-
-          const target = graph.nodes.find((node) => {
-            if (typeof link.target === "number") {
-              return node.id === link.target;
-            } else {
-              return node.id === link.target.id;
-            }
-          });
-
-          if (source === undefined || target === undefined) {
-            return;
-          }
-
-          return (
-            <GraphLink
-              key={index}
-              source={source}
-              target={target}
-              type={link.type}
-            />
-          );
-        })}
-      </g>
-      <g>
-        {graph.nodes.map((node, index) => (
-          <GraphNode key={index} node={node} />
-        ))}
-      </g>
-      <g>
-        {graph.nodes.map((node, index) => (
-          <GraphLabel key={index} node={node} />
-        ))}
-      </g>
-    </svg>
-  );
-}
-
-function GraphNode({ node }: { node: Types.GraphNode }) {
-  function drag(event: React.DragEvent<SVGCircleElement>) {
-    console.log(event);
-  }
-
-  function click() {
-    console.log(node.name);
-  }
-
-  return (
-    <circle
-      r={8}
-      cx={node.x}
-      cy={node.y}
-      onDrag={drag}
-      onClick={click}
-      className="pointer-events-auto"
-    />
-  );
-}
-
-function GraphLabel({ node }: { node: Types.GraphNode }) {
-  return (
-    <text fontSize={15} dx={15} dy={4} x={node.x} y={node.y}>
-      {node.name}
-    </text>
-  );
-}
-
-function GraphLink({
-  source,
-  target,
-  type,
-}: {
-  source: Types.GraphNode;
-  target: Types.GraphNode;
-  type: Types.RelType;
-}) {
-  return (
-    <line
-      strokeWidth={3}
-      stroke={type.color}
-      x1={source.x}
-      x2={target.x}
-      y1={source.y}
-      y2={target.y}
-    />
+    <div
+      ref={wrapperRef}
+      className="pointer-events-none fixed inset-0 h-screen w-screen"
+    >
+      <svg ref={svgRef} className="h-full w-full"></svg>
+    </div>
   );
 }
