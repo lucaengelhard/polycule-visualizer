f0b00c511a113bd9a1a5337d90dc8ecf0cb34a09
Luca Engelhard <me@lucaengelhard.com>
2024-02-27 01:04:10 +0100

    db update event

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import { useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import Graph from "./graph/Graph";
 import UI from "./ui/UI";
 import * as Types from "./types/types";
 import { graph } from "./db/db";
 
 export default function App() {
-  const [graphState, setGraphState] = useState<Types.DBData>(graph);
-  /*
-  setTimeout(() => {
-    const newPerson: Types.GraphNode = {
-      name: "test",
-      id: graphState.nodes.length,
-      location: {
-        name: "Hinterdupfingen",
-        lat: 47.994517,
-        lon: 8.248138,
-      },
-      relationships: [],
-    };
-    setGraphState({ ...graphState, nodes: [...graphState.nodes, newPerson] });
-  }, 3000);*/
+  const [graphState, setGraphState] = useState<Types.GraphData>(graph);
+
+  useEffect(() => {
+    document.addEventListener("onGraphUpdate", () => {
+      setGraphState({
+        ...graph,
+        links: [...graph.links],
+        nodes: [...graph.nodes],
+      });
+    });
+  }, [setGraphState]);
 
   return (
     <>
--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,8 @@
 import { Relationship } from "../classes/relationship";
-import { GraphData, Person } from "../types/types";
 
-const luca: Person = {
+import * as Types from "../types/types";
+
+const luca: Types.Person = {
   id: 0,
   name: "Luca",
   location: {
@@ -12,7 +13,7 @@ const luca: Person = {
   relationships: [],
 };
 
-const luca2: Person = {
+const luca2: Types.Person = {
   id: 2,
   name: "Luca2",
   location: {
@@ -23,7 +24,7 @@ const luca2: Person = {
   relationships: [],
 };
 
-const michi: Person = {
+const michi: Types.Person = {
   id: 1,
   name: "Michi",
   location: {
@@ -40,12 +41,12 @@ const MichiLuca = new Relationship(
   false,
 );
 
-export const db: GraphData = {
+export const db: Types.DBData = {
   nodes: [luca, michi, luca2],
   links: [MichiLuca],
 };
 
-export const graph: GraphData = {
+export const graph: Types.GraphData = {
   nodes: [],
   links: [],
 };
@@ -55,6 +56,9 @@ updateGraphData();
 export function updateGraphData() {
   graph.nodes = db.nodes.map((node) => node);
   graph.links = db.links.map((link) => link);
+
+  const event = new Event("onGraphUpdate");
+  document.dispatchEvent(event);
 }
 
 export function add({
@@ -63,7 +67,7 @@ export function add({
 }: {
   type: "node" | "link";
   payload: typeof type extends "node"
-    ? Person
+    ? Types.Person
     : typeof type extends "link"
       ? Relationship
       : never;
@@ -98,30 +102,30 @@ export function set(input: unknown) {
   }
 }
 
-export function checkGraphDataType(input: unknown): input is GraphData {
-  if ((input as GraphData).links === undefined) {
+export function checkGraphDataType(input: unknown): input is Types.GraphData {
+  if ((input as Types.GraphData).links === undefined) {
     return false;
   }
 
-  if ((input as GraphData).links.length > 0) {
+  if ((input as Types.GraphData).links.length > 0) {
     if (
-      (input as GraphData).links[0].source === undefined ||
-      (input as GraphData).links[0].target === undefined ||
-      (input as GraphData).links[0].distance === undefined
+      (input as Types.GraphData).links[0].source === undefined ||
+      (input as Types.GraphData).links[0].target === undefined ||
+      (input as Types.GraphData).links[0].distance === undefined
     ) {
       return false;
     }
   }
 
-  if ((input as GraphData).nodes === undefined) {
+  if ((input as Types.GraphData).nodes === undefined) {
     return false;
   }
 
-  if ((input as GraphData).nodes.length > 0) {
+  if ((input as Types.GraphData).nodes.length > 0) {
     if (
-      (input as GraphData).nodes[0].name === undefined ||
-      (input as GraphData).nodes[0].id === undefined ||
-      (input as GraphData).nodes[0].location === undefined
+      (input as Types.GraphData).nodes[0].name === undefined ||
+      (input as Types.GraphData).nodes[0].id === undefined ||
+      (input as Types.GraphData).nodes[0].location === undefined
     ) {
       return false;
     }
