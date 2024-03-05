cc8615153d27c11ebf15a6018b685baea3322309
Luca Engelhard <me@lucaengelhard.com>
2024-03-05 14:31:59 +0100

    ui index setup

---
+++ src/ui/index copy.tsx
@@ -0,0 +1,12 @@
+export default function UI() {
+  return (
+    <>
+      <div id="ui-graph"></div>
+      <div id="ui-add"></div>
+      <div id="ui-saveLoad"></div>
+      <div id="ui-edit"></div>
+      <div id="ui-overview"></div>
+      <div id="ui-timeline"></div>
+    </>
+  );
+}
